ce588d5732f44dec83f3b9671c3bfe8e42b83874
baburam bogati <bogatisushma@gmail.com>
2020-08-22 11:50:17 -0500

    account info table created by sushma

---
+++ src/main/java/com/vastika/teamd/bank/model/Account_Info_Table.java
@@ -0,0 +1,42 @@
+package com.vastika.teamd.bank.model;
+
+public class Account_Info_Table {
+	private int id;
+	private String account_name;
+	private String address;
+	private long mobile_number;
+	private long unique_id_type;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAccount_name() {
+		return account_name;
+	}
+	public void setAccount_name(String account_name) {
+		this.account_name = account_name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getMobile_number() {
+		return mobile_number;
+	}
+	public void setMobile_number(long mobile_number) {
+		this.mobile_number = mobile_number;
+	}
+	public long getUnique_id_type() {
+		return unique_id_type;
+	}
+	public void setUnique_id_type(long unique_id_type) {
+		this.unique_id_type = unique_id_type;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/vastika/teamd/bank/model/Customer.java
@@ -0,0 +1,27 @@
+package com.vastika.teamd.bank.model;
+
+public class Customer {
+	private String deposit_amount;
+	private String withdraw_amount;
+	private String balance_account_info_id;
+	public String getDeposit_amount() {
+		return deposit_amount;
+	}
+	public void setDeposit_amount(String deposit_amount) {
+		this.deposit_amount = deposit_amount;
+	}
+	public String getWithdraw_amount() {
+		return withdraw_amount;
+	}
+	public void setWithdraw_amount(String withdraw_amount) {
+		this.withdraw_amount = withdraw_amount;
+	}
+	public String getBalance_account_info_id() {
+		return balance_account_info_id;
+	}
+	public void setBalance_account_info_id(String balance_account_info_id) {
+		this.balance_account_info_id = balance_account_info_id;
+	}
+	
+
+}

ce287ffdcc6d6b688f22c7049f443f73e898da36
Shreya546 <prajapatishreya546@gmail.com>
2020-08-23 00:34:28 -0500

    added file Account_info_tbl class

--- src/main/java/com/vastika/teamd/bank/CreateDbDemo.java
+++ src/main/java/com/vastika/teamd/bank/CreateDbDemo.java
@@ -28,3 +28,7 @@ public class CreateDbDemo {
 	}
 
 }
+//kjdsfjsdbv
+//dvkndkjvnsdkv
+//dvjkbdfkjv
+//dnvkjdfnv,
---
+++ src/main/java/com/vastika/teamd/bank/model/Account_info_tbl.java
@@ -0,0 +1,5 @@
+package com.vastika.teamd.bank.model;
+
+public class Account_info_tbl {
+
+}
